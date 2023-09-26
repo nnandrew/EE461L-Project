5c39b735d339af4a092509ad99ce29621825d1fd
Boris <bhasfire@gmail.com>
2023-09-26 16:21:07 -0500

    very basic GUI for Login page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,22 +1,44 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  // Define state variables to hold the username and password
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  // Define the function to be called when the form is submitted
+  const handleSubmit = (event) => {
+    event.preventDefault(); // Prevent the default form submission behavior
+    console.log('Username:', username);
+    console.log('Password:', password);
+    // Here you would typically send the username and password to your server for authentication
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="username">Username:</label>
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)} // Update the username state variable when the input changes
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password:</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)} // Update the password state variable when the input changes
+            />
+          </div>
+          <button type="submit">Login</button>
+        </form>
       </header>
     </div>
   );
