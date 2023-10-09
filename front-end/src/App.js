3a6c19b2975d7e0d9d82a6be35551e294680d2ac
Eddie Liao <eddieliao03@gmail.com>
2023-10-09 12:53:20 -0500

    Add Google OAuth button

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
 import './App.css';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GoogleLogin } from '@react-oauth/google';
+import { useJwt } from "react-jwt";
 
 function App() {
   // Define state variables to hold the username and password
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-
+  const [token, setToken] = useState('');
   // Define the function to be called when the form is submitted
   const handleSubmit = (event) => {
     event.preventDefault(); // Prevent the default form submission behavior
@@ -14,13 +17,17 @@ function App() {
     // Here you would typically send the username and password to your server for authentication
   };
 
+  const { decodedToken, isExpired } = useJwt(token);
+
+
   return (
+    <GoogleOAuthProvider clientId="612708681336-j44bbu13ctri3t3uc85k5eiok3djbj4b.apps.googleusercontent.com">
     <div className="App">
       <header className="App-header">
         <h1>Login</h1>
         <form onSubmit={handleSubmit}>
           <div>
-            <label htmlFor="username">Username:</label>
+            <label htmlFor="username">Username: </label>
             <input
               type="text"
               id="username"
@@ -29,7 +36,7 @@ function App() {
             />
           </div>
           <div>
-            <label htmlFor="password">Password:</label>
+            <label htmlFor="password">Password: </label>
             <input
               type="password"
               id="password"
@@ -39,8 +46,19 @@ function App() {
           </div>
           <button type="submit">Login</button>
         </form>
+        <GoogleLogin
+          onSuccess={credentialResponse => {
+            console.log(credentialResponse);
+            setToken(credentialResponse.credential);
+            console.log(decodedToken);
+          }}
+          onError={() => {
+            console.log('Login Failed');
+          }}
+        />
       </header>
     </div>
+    </GoogleOAuthProvider>
   );
 }
 
